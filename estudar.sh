aa70a295f7ca5783858a4c99d1333e96563ecb00
gusleaooliveira <gus.leaono@gmail.com>
2020-05-16 16:44:53 -0300

    alteração feita em:16/05/2020-16:44:53

--- estudar.sh
+++ estudar.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-sed -i "s/resources\/css\/hcw.min.css/..\/..\/hcw0\/hcw\/hcw.min.css/" index.html
+sed -i "s/resources\/css\/hcw.min.css/..\/hcw0\/hcw\/hcw.min.css/" index.html
